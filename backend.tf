a2948d5d0cbe2df966ffb95a7e6b361f653fdaa1
Ankur Patel <32640688+ankur51206@users.noreply.github.com>
2024-09-02 17:26:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "realimmerse_group"
-    storage_account_name = "realimmersegroupdiag"
-    container_name       = "tfstate"
-    key                  = "${env.STATE_FOLDER}/terraform.tfstate"
+    # Configuration will be provided during terraform init
   }
 }
