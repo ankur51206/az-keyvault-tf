52ea9e86b7519e953eae94271fea485ee133cd57
Ankur Patel <32640688+ankur51206@users.noreply.github.com>
2024-09-03 09:38:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,26 +6,28 @@ resource "azurerm_resource_group" "example" {
 resource "azurerm_key_vault" "example" {
   name                        = var.key_vault_name
   location                    = azurerm_resource_group.example.location
-  resource_group_name          = azurerm_resource_group.example.name
-  sku_name                     = "standard"  # Other option is "premium"
-  tenant_id                    = var.tenant_id
+  resource_group_name         = azurerm_resource_group.example.name
+  sku_name                    = "standard"  # Other option is "premium"
+  tenant_id                   = var.tenant_id
 
   access_policy {
-    tenant_id = var.tenant_id
-    object_id  = var.object_id
+    tenant_id   = var.tenant_id
+    object_id   = var.object_id
+
     key_permissions = [
-      "get",
-      "list",
-      "create",
-      "delete",
-      "update",
+      "Get",
+      "List",
+      "Create",
+      "Delete",
+      "Update",
     ]
+
     secret_permissions = [
-      "get",
-      "list",
-      "set",
-      "delete",
-      "recover",
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
     ]
   }
 }
